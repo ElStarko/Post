87f5a8d3c91aea59d1d46b125716ea7b40885d1a
ELStarko <114481973+ElStarko@users.noreply.github.com>
2023-03-13 19:06:13 +0100

    6th com

---
+++ routes/comments.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/comment');
+
+// GET all comments for a post
+router.get('/', async (req, res) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).populate('author');
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// CREATE a new comment for a post
+router.post('/', async (req, res) => {
+  const comment = new Comment({
+    content: req.body.content,
+    author: req.body.author,
+    postId: req.params.postId,
+  });
+  try {
+    const newComment = await comment.save();
+    res.status(201).json(newComment);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// UPDATE a comment
+router.put('/', getComment, async (req, res) => {
+  if (req.body.content != null) {
+    res.comment.content = req.body.content;
+  }
+  try {
+    const updatedComment = await res.comment.save();
+    res.json(updatedComment);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE a comment
+router.delete('/', getComment, async (req, res) => {
+  try {
+    await res.comment.remove();
+    res.json({ message: 'Comment deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Middleware function to get comment by ID and attach it to the request object
+async function getComment(req, res, next) {
+  let comment;
+  try {
+    comment = await Comment.findById(req.params.id).populate('author');
+    if (comment == null) {
+      return res.status(404).json({ message: 'Cannot find comment' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.comment = comment;
+  next();
+}
+
+module.exports = router;
--- routes/postits.js
+++ routes/postits.js
@@ -1,83 +1,79 @@
 const express = require('express');
-const Post = require('../models/Post');
+const Post = require('../models/postits');
 
 const router = express.Router();
 
 // GET all posts
-router.get('/posts', async (req, res) => {
-try {
-const posts = await Post.find({ deleted: { $ne: true } }).sort({ createdAt: 'desc' }).populate('author');
-res.status(200).json(posts);
-} catch (err) {
-res.status(500).json({ message: err.message });
-}
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find({ deleted: { $ne: true } }).sort({ createdAt: 'desc' }).populate('author');
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 // GET a specific post by ID
-router.get('/posts/:id', getPost, (req, res) => {
-res.json(res.post);
+router.get('/:id', getPost, (req, res) => {
+    res.json(res.post);
 });
 
 // CREATE a new post
-router.post('/posts', async (req, res) => {
-const post = new Post({
-content: req.body.content,
-media: req.body.media,
-author: req.body.author,
-});
-try {
-const newPost = await post.save();
-res.status(201).json(newPost);
-} catch (err) {
-res.status(400).json({ message: err.message });
-}
+router.post('/', async (req, res) => {
+    const post = new Post({
+        content: req.body.content,
+        author: req.body.author,
+    });
+    try {
+        const newPost = await post.save();
+        res.status(201).json(newPost);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 // UPDATE a post
-router.put('/posts/:id', getPost, async (req, res) => {
-if (req.body.content != null) {
-res.post.content = req.body.content;
-}
-if (req.body.media != null) {
-res.post.media = req.body.media;
-}
-try {
-const updatedPost = await res.post.save();
-res.json(updatedPost);
-} catch (err) {
-res.status(400).json({ message: err.message });
-}
+router.put('/:id', getPost, async (req, res) => {
+    if (req.body.content != null) {
+        res.post.content = req.body.content;
+    }
+   try {
+        const updatedPost = await res.post.save();
+        res.json(updatedPost);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 // SOFT DELETE a post
-router.delete('/posts/:id', getPost, async (req, res) => {
-res.post.deleted = true;
-try {
-const updatedPost = await res.post.save();
-res.json(updatedPost);
-} catch (err) {
-res.status(400).json({ message: err.message });
-}
+router.delete('/:id', getPost, async (req, res) => {
+    res.post.deleted = true;
+    try {
+        const updatedPost = await res.post.save();
+        res.json(updatedPost);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 // Middleware function to get post by ID and attach it to the request object
 async function getPost(req, res, next) {
-let post;
-try {
-post = await Post.findById(req.params.id).populate('author');
-if (post == null) {
-return res.status(404).json({ message: 'Cannot find post' });
-}
-} catch (err) {
-return res.status(500).json({ message: err.message });
-}
+    let post;
+    try {
+        post = await Post.findById(req.params.id).populate('author');
+        if (post == null) {
+            return res.status(404).json({ message: 'Cannot find post' });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
 
-if (post.deleted) {
-return res.status(404).json({ message: 'Cannot find post' });
-}
+    if (post.deleted) {
+        return res.status(404).json({ message: 'Cannot find post' });
+    }
 
-res.post = post;
-next();
+    res.post = post;
+    next();
 }
 
 module.exports = router;
