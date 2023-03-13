380a139bd43811b902c73d1c9335963611712368
ElStarko <114481973+ElStarko@users.noreply.github.com>
2023-03-13 23:13:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 5000;
 initDb();
 app.use(express.json({ extended: false }))
 app.get('/', (req, res) => res.json({ msg: `welcome to this api` }))
+app.get('/api/v1/docs', (req, res) => {
+  res.redirect('https://documenter.getpostman.com/view/26234378/2s93JutNT2');
+});
 
 app.use('/api/postits', require('./routes/postits'))
 app.use('/api/comments', require('./routes/comments'))
